50c02efac6efda861f5cad3399567a2b074d647c
Nikita Aswani <nikitaaswani@Nikitas-MacBook-Pro.local>
2024-07-31 19:26:24 +0530

    Create updates template to render a form to add new pet.

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', async (req, res) => {
     try {
         const resp = await axios.get(petsEndpoint, { headers });
         const pets = resp.data.results;
-        res.render('homepage', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum', pets });
+        res.render('homepage', { pageTitle: 'Custom Object Pets List | Integrating With HubSpot I Practicum', pets });
     } catch (error) {
         console.error(error);
     }
@@ -29,30 +29,19 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update', (req, res) => {
+    try {
+        res.render('update', { pageTitle: 'Update Custom Object Pets Form | Integrating With HubSpot I Practicum'});
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
 
 /**
-* * This is sample code to give you a reference for how you should structure your calls.
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });
-    } catch (error) {
-        console.error(error);
-    }
-});
 
 * * App.post sample
 app.post('/update', async (req, res) => {
